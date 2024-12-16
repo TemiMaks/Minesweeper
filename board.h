92a6c98cc0af4450eec6225bb141f655eecdfcfb
MiKMak <01197826@pw.edu.pl>
2024-12-16 20:16:29 +0100

    Wstepne rysowanie planszy, podzial na funkcje, testowy main

--- board.h
+++ board.h
@@ -1,8 +1,11 @@
-//
-// Created by mikol on 16.12.2024.
-//
 
 #ifndef BOARD_H
 #define BOARD_H
+
+int getRandomNumber(int min, int max);
+void setBoardParams(int level, int *rows, int *cols, int *bombNumber);
+void placeBombs(char **board, int rows, int cols, int bombNumber);
+void showBoard(char **board, int rows, int cols);
+void initializeBoard(int level);
 
 #endif //BOARD_H
--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include "board.h"
 
 int main(void) {
+    int level=2;
+    initializeBoard(level);
 
     return 0;
 }
